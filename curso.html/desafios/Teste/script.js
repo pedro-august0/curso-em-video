5ade6ef7b5046cd600fb4e276e3cd9acace0ffa8
Pedro Augusto Alves Martiniano da Silva <pedro_augusto20161@outlook.com>
2022-06-11 15:09:22 -0300

    Initial commit

---
+++ curso.html/desafios/Teste/script.js
@@ -0,0 +1,5 @@
+alert("CUIDADO! Se não estiver com plena capacidade de participar da reunião, não continue!");
+var sino = (prompt(`Você deseja entrar para a reunião?`) )
+if (sino == "sim") {
+    
+}
